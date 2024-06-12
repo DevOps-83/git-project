405cd3def6c5112f2bdff57a3f0799dfa3bd1f60
Vladimir Dergilev <web_-masrter@mail.ru>
2024-06-12 19:13:59 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+    text := []byte("Пример текста с http://example.com и другими ссылками http://example.org и http://example.net")
+    pattern := []byte("http://")
+
+    for i := 0; i < len(text)-len(pattern); i++ {
+        if string(text[i:i+len(pattern)]) == "http://" {
+            j := i + len(pattern)
+            for j < len(text) && text[j] != ' ' {
+                text[j] = '*'
+                j++
+            }
+            i = j
+        }
+    }
+
+    fmt.Println(string(text))
+}
