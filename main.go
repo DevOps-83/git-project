222d0f43edeea3252ae7aa24eca1538f91ad0112
Vladimir Dergilev <web_-master@mail.ru>
2024-06-14 19:02:31 +0700

    new branch

--- main.go
+++ main.go
@@ -2,20 +2,21 @@ package main
 
 import "fmt"
 
+// test
 func main() {
-    text := []byte("Пример текста с http://example.com и другими ссылками http://example.org и http://example.net")
-    pattern := []byte("http://")
+	text := []byte("Пример текста с http://example.com и другими ссылками http://example.org и http://example.net")
+	pattern := []byte("http://")
 
-    for i := 0; i < len(text)-len(pattern); i++ {
-        if string(text[i:i+len(pattern)]) == "http://" {
-            j := i + len(pattern)
-            for j < len(text) && text[j] != ' ' {
-                text[j] = '*'
-                j++
-            }
-            i = j
-        }
-    }
+	for i := 0; i < len(text)-len(pattern); i++ {
+		if string(text[i:i+len(pattern)]) == "http://" {
+			j := i + len(pattern)
+			for j < len(text) && text[j] != ' ' {
+				text[j] = '*'
+				j++
+			}
+			i = j
+		}
+	}
 
-    fmt.Println(string(text))
+	fmt.Println(string(text))
 }
